f7fad50e38ffebe092f572ef5e8836b64b8f6fc6
Delphine3438 <52829951+Delphine3438@users.noreply.github.com>
2019-08-29 09:54:39 +0200

    Add files via upload

---
+++ exochevalnain.js
@@ -0,0 +1,17 @@
+var cheval = prompt("Quelle est la couleur du cheval blanc d'Henri 4 ?");
+
+  if(cheval==="blanc"){
+var nain = prompt("Combien y a t il de 7 nains ?");
+  }
+
+  else{
+    alert("et Naaaaan");
+  }
+if (nain==="7")
+
+{alert("Yeeeaaah");
+  }
+
+else {
+  alert("t'es nuuuuul!")
+}
---
+++ exoprénom.js
@@ -0,0 +1,9 @@
+var nom = prompt ( "Comment tu t'appelles ?" )
+if ((nom.length > 1) && (nom.length < 10))
+  {
+    alert( "Bonjour " + nom )
+  }
+else
+  {
+    alert( "Va jouer ailleurs !" )
+  }
